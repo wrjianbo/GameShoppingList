0e7e12d57441fdc7a2f488d45ee2308d54582dfe
wrjianbo <jitianbo@tianbodemacbook-pro.local>
2016-03-26 22:54:37 +0000

    Initial Commit

---
+++ GameShoppingList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameShoppingList
+//
+//  Created by Tianbo Ji on 16/3/26.
+//  Copyright © 2016年 Tianbo Ji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
